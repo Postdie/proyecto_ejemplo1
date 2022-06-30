9050bc2c255d709b860be8fc4547feec28e61195
Postdie <fernandezdiego845@gmail.com>
2022-06-29 23:58:44 -0500

    un Commit nomas

---
+++ curso_python/archivos.py
@@ -0,0 +1,20 @@
+def read():
+    numbers = []
+    with open("./archivos/numbers.txt", "r", encoding="utf-8") as f:
+        for line in f:
+            numbers.append(int(line))
+    print(numbers)
+
+def write():
+    names = ["Maria", "Fernanda"]
+    with open("./archivos/names.txt", "a", encoding="utf-8") as f:
+        for names in names:
+            f.write(names)
+            f.write("\n")
+
+def run():
+    write()
+
+
+if __name__ == "__main__":
+    run()
